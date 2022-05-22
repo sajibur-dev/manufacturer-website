3ddaafd801f633179570fbe9fe1a19f5675807bc
sajibur-dev <sajibur.dev@gmail.com>
2022-05-22 13:48:31 +0600

    success page is done

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -1,3 +1,5 @@
+import { faCartArrowDown, faCircleDollarToSlot, faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Tool = ({product}) => {
@@ -10,9 +12,14 @@ const Tool = ({product}) => {
           alt="Shoes"
         />
       </figure>
-      <div class="card-body">
+      <div class="card-body text-accent ">
         <h2 class="card-title">{name}</h2>
-        <p>{shortDescription}</p>
+        <p className="text-sm">{shortDescription}</p>
+        <div className="flex justify-between items-center  my-5">
+          <h4><FontAwesomeIcon className="text-primary" icon={faCartArrowDown}/> : {minimumOrderQuantity} (min)</h4>
+          <h4><FontAwesomeIcon className="text-primary" icon={faDatabase}/> : {availableQuantity} (available)</h4>
+        </div>
+        <h4 className="text-center mb-5"><FontAwesomeIcon className="text-primary" icon={faCircleDollarToSlot}/> : {pricePerProduct} (per product)</h4>
         <div class="card-actions justify-end">
           <button class="btn btn-primary">Buy Now</button>
         </div>
