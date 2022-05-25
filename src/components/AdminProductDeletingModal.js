3f7f09fd91d82b381b861f8d34f7bd4f3488c3bf
sajibur-dev <sajibur.dev@gmail.com>
2022-05-25 10:13:36 +0600

    manage product is done

---
+++ src/components/AdminProductDeletingModal.js
@@ -0,0 +1,46 @@
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { toast } from "react-toastify";
+
+const AdminProductDeletingModal = ({deletingProduct,setDeletingProduct,refetch}) => {
+    const  {_id,name} = deletingProduct;
+const removeItem = (_id) => {
+    fetch(`http://localhost:5000/products/${_id}`,{
+        method:'DELETE',
+        headers:{
+            authorization: `Berer ${localStorage.getItem("accessToken")}`,
+        }
+    }).then((res)=>res.json())
+    .then((result) => {
+        console.log(result);
+        if(result.acknowledged){
+            setDeletingProduct(null)
+            toast.success('successfully deleted');
+            refetch();
+        }
+    })
+}
+  return (
+    <div>
+      <input type="checkbox" id="product-delete-modal" class="modal-toggle" />
+      <div class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box">
+          <h3 class="font-bold text-lg">
+            Are you sure,You wan't delete <span className="text-primary text-2xl">{name}</span>
+          </h3>
+          <div class="modal-action">
+              <button onClick={() => removeItem(_id)}>
+              <FontAwesomeIcon className="text-xl" icon={faTrashCan} />
+              </button>
+            <label for="product-delete-modal" class="btn">
+              cancel
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminProductDeletingModal;
--- src/components/ManageProducts.js
+++ src/components/ManageProducts.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import AdminProductDeletingModal from "./AdminProductDeletingModal";
 
 const ManageProducts = () => {
-    return (
-        <div>
-            manage products
-        </div>
-    );
+  const [deletingProduct, setDeletingProduct] = useState(null);
+  const {
+    data: products,
+    isLoading,
+    refetch,
+  } = useQuery("products", () =>
+    fetch("http://localhost:5000/products").then((res) => res.json())
+  );
+  return (
+    <div className="lg:w-full">
+      <h1 className="text-2xl text-center my-5">all products ...</h1>
+      <div class="overflow-x-auto ">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>image</th>
+              <th>name</th>
+              <th>action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products?.map((product, i) => (
+              <tr key={product._id}>
+                <th>{i + 1}</th>
+                <td>
+                  <div class="avatar">
+                    <div class="lg:w-24 w-12 rounded-full">
+                      <img src={product?.image} alt="product" />
+                    </div>
+                  </div>
+                </td>
+                <td>{product?.name}</td>
+                <td>
+                  <label
+                    for="product-delete-modal"
+                    onClick={() => setDeletingProduct(product)}
+                    className="btn btn-ghost hover:text-red-500"
+                  >
+                    <FontAwesomeIcon className="text-xl" icon={faTrashCan} />
+                  </label>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {deletingProduct && (
+          <AdminProductDeletingModal
+            deletingProduct={deletingProduct}
+            setDeletingProduct={setDeletingProduct}
+            refetch={refetch}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ManageProducts;
